439ae7e0696d1727c3f4a3a75e86bbccc0c9817b
Indra2108 <fizipratama21@gmail.com>
2022-06-27 03:12:24 +0700

    Routes: Add /{any*} path

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,40 @@
+const login = require("../handler/LOGIN")
+const register = require("../handler/REGISTER")
+
 const routes = [
     {
         method: 'GET',
-        path: '/',
+        path: '/restricted',
         handler: (request, h) => {
-            return 'Hello gaes!'
+            return 'Selamat anda berhasil masuk menggunakan jwt token'
         }
-    }
+    },
+    {
+        method: 'POST',
+        path: '/register',
+        handler: register,
+        options: {
+            auth: false
+        }
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: login,
+        options: {
+            auth: false
+        }
+    },
+    {
+        method: 'GET',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Nyasar bossque!'
+        },
+        options: {
+            auth: false
+        }
+    },
 ]
 
 module.exports = routes
