a6cb85ea65785cdd13c2f38cf3efd1e37709fc06
Indra2108 <fizipratama21@gmail.com>
2022-06-27 03:13:03 +0700

    Server: Configure jwt auth strategy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const Hapi = require('@hapi/hapi')
+const jwt = require('jsonwebtoken')
 
+const User = require('./models')
 const plugins = require('./plugins')
 const routes = require('./routes')
 
@@ -13,6 +15,21 @@ const init = async () => {
     })
 
     await server.register(plugins)
+
+    server.auth.strategy('simple', 'bearer-access-token', {
+        validate: async (request, token, h) => {
+            const decoded = jwt.verify(token, 'rahasia')
+
+            const userjwt = await User.findByPk(decoded.id)
+
+            if (!userjwt) {
+                return { isValid: false, credentials: null }
+            }
+
+            return { isValid: true, credentials: { id: userjwt.id } }
+        }
+    })
+    server.auth.default('simple')
 
     server.route(routes)
 
